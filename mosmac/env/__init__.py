b5a56228ba6555f09901bfb3e3285b9f1e0b8477
GENG Minghong <58876849+GENGMinghong@users.noreply.github.com>
2024-06-16 02:00:46 +0800

    Revert "Upload mosmac"
    This reverts commit 63d5310ddf814af2e32075ffe3f02c5f94f15d7f.

--- mosmac/env/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from mosmac.env.multiagentenv import MultiAgentEnv
-from mosmac.env.starcraft2.starcraft2_HC import StarCraft2EnvSMANC, StarCraft2EnvLMANC
-
-__all__ = ["MultiAgentEnv", "StarCraft2EnvSMANC", "StarCraft2EnvLMANC", ]
